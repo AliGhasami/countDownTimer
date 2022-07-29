b911953ebc5c173d438214bf9294c71418a7a499
Ali Ghasami <alighassami@yahoo.com>
2022-07-29 19:55:56 +0430

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    this is count down timer {{ time }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import useCountdownTimer from "@/useCountdownTimer";
+const {reset,time,start,stop,onFinish} = useCountdownTimer('00:01:00',"HH:mm:ss")
+start()
+</script>
+
+<style>
+</style>
---
+++ src/useCountdownTimer.ts
@@ -0,0 +1,58 @@
+import { onMounted, ref } from "vue";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import moment from "moment";
+//sample 00:30 -  1:50 - 00:59
+export default function useCountdownTimer(
+  _time = "00:00:30",
+  format = "HH:mm:ss"
+) {
+  let interval: number;
+  let startTime = moment(_time, "HH:mm:ss");
+  //const temp=
+  const endTime = moment("00:00:00", "HH:mm:ss");
+  const time = ref("");
+  let tempHook: () => void;
+
+  function decrement() {
+    if (startTime > endTime) {
+      startTime.subtract("1", "second");
+      time.value = startTime.format(format);
+    } else {
+      clearInterval(interval);
+      if (tempHook) {
+        tempHook();
+      }
+    }
+  }
+
+  function start() {
+    interval = setInterval(decrement, 1000);
+  }
+
+  function stop() {
+    clearInterval(interval);
+  }
+
+  function onFinish(hook: () => any) {
+    tempHook = hook;
+  }
+
+  function reset() {
+    startTime = moment(_time, "HH:mm:ss");
+    stop();
+    //start()
+  }
+
+  /* function setTime(_time: string) {
+    startTime = moment(_time, 'HH:mm:ss')
+    stop()
+    start()
+  }*/
+
+  onMounted(() => {
+    time.value = startTime.format(format);
+  });
+
+  return { time, stop, start, onFinish, reset };
+}
